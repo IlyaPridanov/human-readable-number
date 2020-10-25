52f199de12aba58c3f11bb97683e1533bafad594
Ilya Pridanov <pridanov.ilya@mail.ru>
2020-10-25 17:56:04 +0400

    fix:name

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 module.exports = function toReadable (number) {
     let result = '';
-    const units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-    const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-    const tens = ['twenty ', 'thirty ', 'forty ', 'fifty ', 'sixty ', 'seventy ', 'eighty ', 'ninety '];
+    const digit = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const teen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const ten = ['twenty ', 'thirty ', 'forty ', 'fifty ', 'sixty ', 'seventy ', 'eighty ', 'ninety '];
 
     const str = number.toString();
     result = str.split('').map((el, i) => {
@@ -10,13 +10,13 @@ module.exports = function toReadable (number) {
         if (str.length === 1 && str[0] === '0') {
             newEl = 'zero';
         } else if (str.length === 3 && i === 0) {
-            newEl = `${units[el]} hundred `;
+            newEl = `${digit[el]} hundred `;
         } else if (str.length > 1 && str[str.length - 2] !== '1' && i === str.length - 2) {
-            newEl = tens[el - 2];
+            newEl = ten[el - 2];
         } else if (str.length > 1 && str[str.length - 2] === '1' && i === str.length - 1) {
-            newEl = teens[el] || '';
+            newEl = teen[el] || '';
         } else if (str[str.length - 2] !== '1' && i === str.length - 1) {
-            newEl = units[el];
+            newEl = digit[el];
         }
         return newEl;
     }).join('');
